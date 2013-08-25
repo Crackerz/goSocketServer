6aa92b5ff9e900b081c0ff66c7cd9fb806897fab
William Blankenship <william.jblankenship@gmail.com>
2013-08-24 21:27:22 -0400

    Added connect and disconnect messages

--- socket.go
+++ socket.go
@@ -11,34 +11,31 @@ type Socket struct {
 	id int
 }
 
-func (s Socket) Handle() {
-	//Register socket with server
-	s.id = Server.add(s)
-	io.Copy(s,s.Connection)
-	Server.remove(s.id)
+func (s *Socket) Handle() {
+	(*s).Register()
+	io.Copy(*s,(*s).Connection) //Blocking function to handle all communication
+	(*s).Disconnect()
+}
+
+//Register the socket with the server
+func (s *Socket) Register() {
+	(*s).id = Server.add(*s)
+	Server.WriteAll("Anon "+strconv.Itoa((*s).id)+" joined")
+}
+
+//Cleanup server after loosing connection with socket
+func (s *Socket) Disconnect() {
+	Server.remove((*s).id)
+	Server.WriteAll("Anon "+strconv.Itoa((*s).id)+" disconnected")
 }
 
 func (s Socket) Write(p []byte) (n int, err error) {
 	fmt.Printf("Write Called\n")
-	//fmt.Fprintf(s.Connection,s.getResp(string(p)))
 	Server.WriteAll("Anon "+strconv.Itoa(s.id)+":"+string(p))
 	fmt.Printf("Write Completed\n")
 	return len(p), nil
 }
 
-func (s Socket) WriteString(message string) {
-	fmt.Fprintf(s.Connection,message)
-}
-
-func (s Socket) getResp(req string) string {
-	fmt.Printf("getResp Called\n")
-	switch req {
-	case "Name":
-		return "GoLang"
-	case "Author":
-		return "William Blankenship"
-	case "Univ.":
-		return "SIUC"
-	}
-	return "Unknown Query"
+func (s *Socket) WriteString(message string) {
+	fmt.Fprintf((*s).Connection,message)
 }
