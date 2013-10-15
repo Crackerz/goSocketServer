6590a47d476b9f85c09515e55378cb5bc2c2e35b
William Blankenship <william.jblankenship@gmail.com>
2013-10-15 19:27:47 -0400

    Commented file

--- server.go
+++ server.go
@@ -1,13 +1,22 @@
+/*
+goSocketServer is a simple wrapper for the go.net/websocket library
+*/
 package goSocketServer
 
+/*
+SocketServer manages connecting, disconnecting, and handling messages from all websockets registered with it.
+*/
 type SocketServer struct {
-	sockets map[int]*Socket
-	uniq_id int
+	sockets map[int]*Socket //a map of all Sockets the server is monitoring w/ their IDs as keys.
+	uniq_id int //Used for generating unique IDs
 	onConnect func(*Socket)
 	onDisconnect func(*Socket)
 	onMessage func(*Socket,[]byte)
 }
 
+/*
+Server is the default SocketServer for this library. Unless you know what you are doing, it is recommended you use this default server for all websockets in your application.
+*/
 var Server SocketServer
 
 func init() {
@@ -15,6 +24,9 @@ func init() {
 	Server.uniq_id = 0
 }
 
+/*
+add() causes the server to start monitoring a websocket wrapped in this library's Socket struct.
+*/
 func (s *SocketServer) add(socket Socket) int {
 	s.sockets[s.uniq_id] = &socket
 	socket.id = s.uniq_id
@@ -25,28 +37,43 @@ func (s *SocketServer) add(socket Socket) int {
 	return Server.uniq_id-1
 }
 
-func (s *SocketServer) remove(index int) {
-	socket:=s.sockets[index]
+/*
+remove() stops the server from monitoring a websocket with the specified.
+*/
+func (s *SocketServer) remove(id int) {
+	socket:=s.sockets[id]
 	if s.onDisconnect!=nil {
 		s.onDisconnect(socket)
 	}
-	delete(s.sockets,index)
+	delete(s.sockets,id)
 }
 
+/*
+WriteAll() sends the string "message" to all sockets currently connected to the server.
+*/
 func (s *SocketServer) WriteAll(message string) {
 	for _,socket := range s.sockets {
 		socket.SendString(message)
 	}
 }
 
+/*
+OnConnect() sets a callback function that will be called whenever a new websocket connects to the server. The connecting websocket will be wrapped in this library's Socket struct and passed to the callback function as a parameter.
+*/
 func (s *SocketServer) OnConnect(function func(*Socket)) {
 	s.onConnect = function
 }
 
+/*
+OnDisconnect() sets a callback function that will be called whenever a websocket disconnects from the server. The Socket struct mapped to this websocket will be passed to the callback function as a parameter.
+*/
 func (s *SocketServer) OnDisconnect(function func(*Socket)) {
 	s.onDisconnect = function
 }
 
+/*
+OnMessage() sets a callback function that will be called whenever the server recieves a message from the websocket. The Socket struct mapped to the websocket that sent a message and the []byte array representing the message will be passed to the callback function as parameters.
+*/
 func (s *SocketServer) OnMessage(function func(*Socket, []byte)) {
 	s.onMessage = function
 }
--- socket.go
+++ socket.go
@@ -5,31 +5,47 @@ import ("fmt"
 	"code.google.com/p/go.net/websocket"
 )
 
+/*
+Socket is a simple structure that wraps the functionality of a websocket.Conn, allowing a websocket to be registered with a server in this library.
+*/
 type Socket struct {
 	Connection *websocket.Conn
 	id int
 }
 
+/*
+Handle() is a blocking function that handles all communication with the websocket as defined by the callback functions provided to the server. When calling Handle() as a thread, take care to ensure the websocket is not closed by your application.
+*/
 func (s *Socket) Handle() {
 	s.Register()
 	io.Copy(s,s.Connection) //Blocking function to handle all communication
 	s.Disconnect()
 }
 
+/*
+GetID() returns the unique id of the websocket.
+*/
 func (s *Socket) GetId() int {
 	return s.id
 }
 
-//Register the socket with the server
+/*
+Register() causes the default server in this library to start monitoring the Socket
+*/
 func (s *Socket) Register() {
 	s.id = Server.add(*s)
 }
 
-//Cleanup server after loosing connection with socket
+/*
+Disconnect() causes the default server in this library to stop monitoring the Socket
+*/
 func (s *Socket) Disconnect() {
 	Server.remove(s.id)
 }
 
+/*
+Write() is called when a message is received from the websocket. Write() DOES NOT send a message to the websocket. To send a message to the websocket use SendBytes() or SendString().
+*/
 func (s *Socket) Write(p []byte) (n int, err error) {
 	if Server.onMessage != nil {
 		Server.onMessage(s,p)
@@ -37,14 +53,23 @@ func (s *Socket) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+/*
+SendString() sends a message to the websocket.
+*/
 func (s *Socket) SendString(message string) {
 	fmt.Fprintf(s.Connection,message)
 }
 
+/*
+SendBytes() sends a message to the websocket.
+*/
 func (s *Socket) SendBytes(message []byte) {
 	fmt.Fprintf(s.Connection,"%s",message)
 }
 
+/*
+NewSocket() creates a new Socket structure that wraps the websocket. The unique ID will be 0 and is assigned when the Socket is registered with a server.
+*/
 func NewSocket(ws *websocket.Conn) Socket {
 	return Socket{ws,0}
 }
